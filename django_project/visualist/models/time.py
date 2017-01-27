97a2b88ab986025d6e53049e64920f55b9c57731
Allan Berry <allan.berry@gmail.com>
2017-01-26 22:18:55 -0600

    Add lecture to time.

--- django_project/visualist/models/time.py
+++ django_project/visualist/models/time.py
@@ -21,6 +21,7 @@ class Event(Record):
         ("EXHIBITION", "exhibition"),
         ("PERFORMANCE", "performance"),
         ("WORKSHOP", "workshop"),
+        ("LECTURE", "lecture")
     )
     record_type = models.CharField(max_length=20,
         choices=TYPES,
