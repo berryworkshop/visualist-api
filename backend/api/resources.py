1e3710b8d0d4b04e32cf247e0e49cbbbd21b497f
Allan Berry <allan.berry@gmail.com>
2017-04-28 22:55:14 -0500

    putzing

--- backend/api/resources.py
+++ backend/api/resources.py
@@ -1,29 +1,14 @@
 from flask import abort, request, jsonify
-from flask_restful import Resource, reqparse#, fields, marshal
+from flask_restful import Resource
 from py2neo import Graph, Node
-from marshmallow import Schema, fields, pprint
+from marshmallow import Schema
 
 
-from .models import EventSchema
+from .schemas import EventSchema
 graph = Graph()
 
 
-def get_highest_event_id():
-    highest_id = graph.run('''
-        MATCH (e:Event)
-        WHERE exists(e.id)
-        RETURN e.id
-        ORDER BY e.id DESC
-        LIMIT 1
-        ''').evaluate()
-
-    if highest_id:
-        return highest_id
-    else:
-        return 0
-
 class EventResource(Resource):
-
     def __init__(self):
         self.schema = EventSchema()
 
@@ -34,10 +19,13 @@ class EventResource(Resource):
         return dict(event)
 
     def put(self, id):
+        errors = self.schema.validate(request.json)
+        if errors:
+            abort(400, {'validation errors': errors })
         event = graph.find_one('Event', 'id', id)
         if not event:
             abort(404)
-        for k, v in args.items():
+        for k, v in request.json.items():
             if v != None:
                 event[k] = v
         graph.push(event)
@@ -48,10 +36,9 @@ class EventResource(Resource):
         if not event:
             abort(404)
         graph.delete(event)
-        return jsonify({ 'result': True })
+        return 'event {} permanently deleted'.format(id)
 
 class EventListResource(Resource):
-
     def __init__(self):
         self.schema = EventSchema()
 
@@ -64,9 +51,16 @@ class EventListResource(Resource):
     def post(self):
         event, errors = self.schema.load(request.json)
         if errors:
-            abort(400, 'The submitted content does not pass validation.  Please check for spelling errors or missing fields.')
+            abort(400, {'validation errors': errors })
+        event = Node("Event", **request.json)
+        graph.create(event)
+        return dict(event)
 
-        data, errors = self.schema.dump(event)
-        event_node = Node("Event", **data)
-        graph.create(event_node)
-        return {'event': dict(event_node)}
+    def delete(self):
+        abort(404) # this is a dangerous method for sure
+        events = graph.find('Event')
+        if not events:
+            abort(404)
+        for event in events:
+            graph.delete(event)
+        return 'all events permanently deleted'
--- backend/api/schemas.py
+++ backend/api/schemas.py
@@ -3,11 +3,11 @@ from marshmallow import Schema, fields, post_load
 
 
 class Event(object):
-    def __init__(self, *args, **kwargs):
-        self.name = kwargs.get('name')
-        self.category = kwargs.get('category')
-        self.description = kwargs.get('description')
-        self.uri = kwargs.get('uri')
+    def __init__(self, name, category, description):
+        # self.slug = slug
+        self.name = name
+        self.category = category
+        self.description = description
         self.created_at = dt.datetime.now()
 
     def __repr__(self):
@@ -15,10 +15,10 @@ class Event(object):
 
 
 class EventSchema(Schema):
-    pk = fields.Integer()
+    # slug = fields.String(required=True)
     name = fields.String(required=True)
     category = fields.String(required=True)
-    uri = fields.Url()
+    # uri = fields.Url()
     description = fields.String()
 
     @post_load
