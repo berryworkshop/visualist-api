3ec3ce6ddad7c750de25e9697c5c1f5d008b3d37
Allan Berry <allan.berry@gmail.com>
2017-06-06 20:34:20 -0500

    working docker compose (from sketchbook)

---
+++ gunicorn.py
@@ -0,0 +1,13 @@
+# gunicorn.py
+import os
+
+if os.environ.get('SETTINGS_MODE') == 'development':
+    reload = True
+    loglevel = 'debug'
+    accesslog = '-'
+elif os.environ.get('SETTINGS_MODE') == 'test':
+    pass
+else: # 'production'
+    pass
+
+bind = '0.0.0.0:8000'
---
+++ project/app.py
@@ -0,0 +1,46 @@
+import os, sys
+from flask import Flask, jsonify
+from py2neo import authenticate, Graph, Relationship
+
+app = Flask(__name__)
+
+app.config['NEO4J_USER']     = os.getenv('NEO4J_USER', 'neo4j')
+app.config['NEO4J_PASSWORD'] = os.getenv('NEO4J_PASSWORD')
+
+@app.route('/')
+def hello_world():
+    return jsonify({
+        'Hello': 'world!'
+    })
+
+@app.route('/db')
+def hello_db():
+    db = Graph(
+        host='db',
+        user=app.config['NEO4J_USER'],
+        password=app.config['NEO4J_PASSWORD']
+    )
+    return jsonify(db.data("MATCH (a) RETURN a"))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({
+        "message": "resource not found"
+    }), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return jsonify({
+        "message": "server error"
+    }), 500
+
+
+
+# def configure_app(app):
+#     config = {
+#         "development": DevelopmentConfig,
+#         "testing": TestingConfig,
+#         "default": ProductionConfig,
+#     }
+#     config_name = os.getenv('FLASK_CONFIGURATION', 'default')
+#     app.config.from_object(config[config_name])
