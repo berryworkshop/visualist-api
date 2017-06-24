c885ab1d8281103e886fabd3a292659a951e4264
Allan Berry <allan.berry@gmail.com>
2017-06-24 14:48:49 -0500

    basic setup; tests pass

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,12 @@
 class _BaseConfig:
     DEBUG = False
     TESTING = False
+    GRAPH_DATABASE = 'http://localhost:7474/db/data/'
+    GRAPH_USER = 'neo4j'
+    GRAPH_PASSWORD = 'abcd1234'
+
+    # TODO: Override this as a default when using Neo4j as a data layer
+    ITEM_URL = 'regex("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")'
 
 
 class DevelopmentConfig(_BaseConfig):
--- api/tests/test_base.py
+++ api/tests/test_base.py
@@ -1,70 +1,40 @@
+import os
 import requests
-import app as visualist
+from py2neo import Graph
+from base64 import b64encode
 from unittest import TestCase
+from ..project import app as visualist
 
 
 class BaseTestCase(TestCase):
-    '''
-    base url should look like this:
-    {
-      "links": {
-        "events": "/events/"
-      }
-    }
-    '''
 
     def setUp(self):
-        # self.database_url = 'http://0.0.0.0:7474/db/data/'
-        # self.api_base_url = 'http://localhost:5000/'
-        # self.sub_views = [
-        #     'events',
-        # ]
+        self.database_url = 'http://localhost:7474/db/data/'
+        self.api_url = 'http://localhost:8000/'
+        self.sub_views = [
+            'events',
+        ]
+        self.graph = Graph(
+            self.database_url, password='test_database_password')
 
-        visualist.configure_app(visualist.app)
-        self.app = visualist.app.test_client()
-        visualist.init_db()
-
-        # Maybe someday I'll get docker running through Python
-        # https://docs.docker.com/engine/reference/commandline/run/
-        # client = docker.from_env()
-        # docker run --rm -p 7474:7474 -p 7687:7687  -e 'NEO4J_AUTH=none' -v $HOME/neo4j/data:/data neo4j:3.1
-        # self.container = client.containers.run("neo4j",
-        #     detach=True,
-        #     environment={'NEO4J_AUTH':'none'},
-        #     ports={7474:7474, 7687:7687},
-        #     volumes={'/Users/aljabear/neo4j/data': {'bind': '/data', 'mode': 'rw'}}
-        # )
-        # self.container.start()
 
     def tearDown(self):
-        # visualist.stop_db(self.app)
         pass
 
+    def test_api_loads(self):
+        r = requests.get(self.api_url)
+        self.assertEqual(r.status_code, 200)
 
-    def test_base_url_loads(self):
-        # r = requests.get(self.api_base_url)
-        # self.assertEqual(r.status_code, 200)
-        pass
+    def test_database_loads_no_auth(self):
+        r = requests.get(self.database_url)
+        self.assertEqual(r.status_code, 401)
 
-    # def test_database_loads(self):
-    #     r = requests.get(self.database_url)
-    #     self.assertEqual(r.status_code, 200)
+    def test_database_loads_with_auth(self):
+        r = requests.get(self.database_url, auth=(
+            'neo4j','test_database_password'))
+        self.assertEqual(r.status_code, 200)
 
-    # def test_base_url_contains_links(self):
-    #     r = requests.get(self.api_base_url)
-    #     self.assertTrue('links' in r.json())
+    def test_database_is_empty(self):
+        r = self.graph.data("MATCH (a) RETURN a")
+        self.assertFalse(r)
 
-    # def test_links_contains_events(self):
-    #     r = requests.get(self.api_base_url)
-    #     self.assertTrue('events' in r.json().get('links'))
-
-    # def test_link_list_contains_all_views(self):
-    #     r = requests.get(self.api_base_url)
-    #     link_list = r.json().get('links')
-    #     for v in self.sub_views:
-    #         self.assertTrue(v in link_list)
-
-    # def test_link_list_contains_same_number_of_views_as_sub_views(self):
-    #     r = requests.get(self.api_base_url)
-    #     link_list = r.json().get('links')
-    #     self.assertEqual(len(self.sub_views), len(link_list))
