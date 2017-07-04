8b235d23b31e7c9457f6ec8a3e9be179c889b9db
Allan Berry <allan.berry@gmail.com>
2017-07-03 22:17:41 -0500

    start of database tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,9 +1,5 @@
-import os
 import requests
-from py2neo import Graph
-from base64 import b64encode
 from unittest import TestCase
-from ..project import app as visualist
 
 
 class BaseTestCase(TestCase):
@@ -14,8 +10,6 @@ class BaseTestCase(TestCase):
         self.sub_views = [
             'events',
         ]
-        self.graph = Graph(
-            self.database_url, password='test_database_password')
 
     def tearDown(self):
         pass
@@ -23,17 +17,4 @@ class BaseTestCase(TestCase):
     def test_api_loads(self):
         r = requests.get(self.api_url)
         self.assertEqual(r.status_code, 200)
-
-    def test_database_loads_no_auth(self):
-        r = requests.get(self.database_url)
-        self.assertEqual(r.status_code, 401)
-
-    def test_database_loads_with_auth(self):
-        r = requests.get(self.database_url, auth=(
-            'neo4j','test_database_password'))
-        self.assertEqual(r.status_code, 200)
-
-    def test_database_is_empty(self):
-        r = self.graph.data("MATCH (a) RETURN a")
-        self.assertFalse(r)
 
