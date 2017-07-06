0a3e61571093a4096aa2a4fc12c8a9e832de81fe
Allan Berry <allan.berry@gmail.com>
2017-07-06 02:37:24 +0000

    reconfigured so digital ocean works

--- api/gunicorn_config.py
+++ api/gunicorn_config.py
@@ -4,7 +4,7 @@ if os.environ.get('SETTINGS_MODE') == 'development':
     reload = True
     loglevel = 'debug'
     accesslog = '-'
-    bind = '0.0.0.0:8000'
+    #bind = '0.0.0.0:8000'
 elif os.environ.get('SETTINGS_MODE') == 'test':
     pass
 else: # 'production'
