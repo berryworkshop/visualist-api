3c926d72c5d679bdb5f65222bfe997c8f6ae3ca0
Allan Berry <allan.berry@gmail.com>
2017-07-09 08:01:43 -0500

    progress; settings package works as flask and gunicorn; docker not quite.

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,13 +1,12 @@
 import os
 import pymongo
-from .settings import development, production
 from eve import Eve
+from .settings import development, production
 
 if (os.getenv('SETTINGS_MODE') == 'development'):
     app = Eve(settings=development.settings)
 else:
     app = Eve(settings=production.settings)
-
 
 
 if __name__ == '__main__':
