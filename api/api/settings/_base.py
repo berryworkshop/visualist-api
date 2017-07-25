cf37093971f4c0376784887cb5893002685f1db1
Allan Berry <allan.berry@gmail.com>
2017-07-25 08:05:41 -0500

    api working for people

--- api/api/settings/_base.py
+++ api/api/settings/_base.py
@@ -1,4 +1,6 @@
 import os
+import yaml
+import pkg_resources
 
 db_user   = os.getenv('MONGO_USERNAME', 'visualist_admin')
 db_pass   = os.getenv('MONGO_PASSWORD')
@@ -6,8 +8,19 @@ db_server = os.getenv('MONGO_SERVER', 'localhost')
 db_port   = os.getenv('MONGO_PORT', 27017)
 db_name   = os.getenv('MONGO_DBNAME', 'visualist')
 
+s = pkg_resources.resource_string(__name__, '/schema.yaml')
+schema = yaml.load(s)
+
+people = {
+    'item_title': 'person',
+    'resource_methods': ['GET', 'POST'],
+    'schema': schema['PersonSchema'],
+}
+
 settings = {
-    'DOMAIN': {'people': {}},
+    'DOMAIN': {'people': people},
     'XML': False,
     'MONGO_URI': f'mongodb://{db_user}:{db_pass}@{db_server}:{db_port}/{db_name}',
+    'RESOURCE_METHODS': ['GET', 'POST', 'DELETE'],
+    'ITEM_METHODS': ['GET', 'PATCH', 'PUT', 'DELETE'],
 }
