a714618cae6b57e1a4d1581df8e7676f793f7de6
Allan Berry <allan.berry@gmail.com>
2017-09-09 13:34:48 -0500

    debugging

--- visualist/settings/_base.py
+++ visualist/settings/_base.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'api',
 ]
 
@@ -68,3 +69,5 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+STATIC_URL = '/static/'
