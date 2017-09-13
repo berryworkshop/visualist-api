cb1a9bdbc77a8da6ba6ed30420aa9da87031c2a7
Allan Berry <allan.berry@gmail.com>
2017-09-13 12:08:39 -0500

    need local postgres

--- api/admin.py
+++ api/admin.py
@@ -2,37 +2,41 @@ from django.contrib import admin
 from api.models import (
     Address,
     Term,
-    Collection,
+    # Collection,
     Email,
-    Event,
+    # Event,
     Identifier,
     Image,
-    Organization,
-    Page,
-    Person,
+    # Organization,
+    # Page,
+    # Person,
     Phone,
-    Place,
-    Post,
-    Snippet,
+    # Place,
+    # Post,
+    # Snippet,
     SocialAccount,
     Tag,
-    Work,
+    # Work,
+    Record,
+    Relation,
 )
 
 admin.site.register(Address)
 admin.site.register(Term)
-admin.site.register(Collection)
+# admin.site.register(Collection)
 admin.site.register(Email)
-admin.site.register(Event)
+# admin.site.register(Event)
 admin.site.register(Identifier)
 admin.site.register(Image)
-admin.site.register(Organization)
-admin.site.register(Page)
-admin.site.register(Person)
+# admin.site.register(Organization)
+# admin.site.register(Page)
+# admin.site.register(Person)
 admin.site.register(Phone)
-admin.site.register(Place)
-admin.site.register(Post)
-admin.site.register(Snippet)
+# admin.site.register(Place)
+# admin.site.register(Post)
+# admin.site.register(Snippet)
 admin.site.register(SocialAccount)
 admin.site.register(Tag)
-admin.site.register(Work)
+admin.site.register(Record)
+admin.site.register(Relation)
+# admin.site.register(Work)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres import fields as pg
 
 # todo:
     # PyDoc for classes
@@ -9,11 +10,23 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
-    created = models.DateTimeField(
+    created_on = models.DateTimeField(
         auto_now_add=True,
     )
-    modified = models.DateTimeField(
+    modified_on = models.DateTimeField(
         auto_now=True,
+    )
+
+
+class Resource(Base):
+    title = models.CharField(
+        max_length=250,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+    url = models.URLField(
+        blank=True,
     )
 
 
@@ -21,7 +34,7 @@ class Sourced(models.Model):
     class Meta:
         abstract = True
 
-    source = models.ForeignKey('Work',
+    source = models.ForeignKey('Resource',
         blank=True,
     )
 
@@ -35,6 +48,21 @@ class Hierarchical(models.Model):
         blank=True,
         null=True,
     )
+
+
+class Name(Base, Sourced):
+
+    record = models.ForeignKey('Record')
+    primary = models.CharField(
+        max_length = 250,
+    )
+    secondary = models.CharField(
+        max_length = 250,
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.value
 
 
 class Image(Base, Sourced):
@@ -170,26 +198,14 @@ class SocialAccount(Base, Sourced):
         return '{}: {}'.format(self.service, self.value)
 
 
-class Snippet(Base, Sourced):
-
-    value = models.TextField()
-    license = models.ForeignKey('Work',
-        related_name='licensed_snippets'
-    )
-
-    def __str__(self):
-        return self.value
-
-
 class Term(Base, Hierarchical):
     class Meta:
-        ordering = ['value']
         unique_together = (
             ("value", "vocabulary"),
             ("value", "parent"),
         )
 
-    vocabulary = models.ForeignKey('Work')
+    vocabulary = models.ForeignKey('Resource')
     value = models.CharField(
         max_length=250,
     )
@@ -199,8 +215,6 @@ class Term(Base, Hierarchical):
 
 
 class Tag(Base):
-    class Meta:
-        ordering = ['value']
 
     value = models.SlugField(
         unique=True
@@ -211,8 +225,6 @@ class Tag(Base):
 
 
 class Identifier(Base):
-    class Meta:
-        ordering = ['-created']
 
     value = models.CharField(
         max_length=250,
@@ -223,135 +235,158 @@ class Identifier(Base):
         return self.value
 
 
-
-CATEGORIES = {
-    'event': [
-        'course',
-        'exhibition',
-        'performance',
-        'reception',
-        'residency',
-        'workshop',
-    ],
-    'work': [
-        'article',
-        'book',
-        'installation',
-        'photograph',
-        'sculpture',
-        'visual artwork',
-        'website',
-        'vocabulary',
-        'license',
-    ],
-    'person': [
-        'artist',
-        'writer',
-        'architect',
-        'filmmaker',
-        'curator',
-        'gallerist',
-        'professor',
-    ],
-    'organization': [
-        'archive',
-        'association',
-        'company',
-        'consortium',
-        'foundation',
-        'library',
-        'museum',
-        'school',
-    ],
-    'page': [
-        'article',
-        'review',
-    ],
-    'collection': [
-        'tour'
-    ],
-}
-
-
-
-class Label(models.Model):
-    pass
-
-
-
 class Relation(models.Model):
+
     PREDICATES = (
-        (('parent'), ('parent of')),
-        (('category'), ('category for')),
-        (('member'), ('member of')),
-
-        # general
-        (('creator'), ('creator of')),
-        (('contributor'), ('contributor to')),
-
-        # event
-        (('organizer'), ('organizer of')),
-        (('curator'), ('curator of')),
-        (('exhibitor'), ('exhibitor of')),
-        (('producer'), ('producer of')),
-
-        # work
-        (('publisher'), ('publisher of')),
+        (('has_category'), ('has category')),
+        (('has_contributor'), ('has contributor')),
+        (('has_creator'), ('has creator')),
+        (('has_curator'), ('has curator')),
+        (('has_employee'), ('has employee')),
+        (('has_exhibitor'), ('has exhibitor')),
+        (('has_friend'), ('has friend')),
+        (('has_member'), ('has member')),
+        (('has_organizer'), ('has organizer')),
+        (('has_owner'), ('has owner')),
+        (('has_parent'), ('has parent')),
+        (('has_place'), ('has place')),
+        (('has_producer'), ('has producer')),
+        (('has_publisher'), ('has publisher')),
     )
-    subject = models.ForeignKey(Record, on_delete=models.PROTECT)
+    subject = models.ForeignKey('Record',
+        related_name='relation_subject',
+        on_delete=models.PROTECT,
+    )
     predicate = models.CharField(
-        options=PREDICATES,
+        choices=PREDICATES,
+        max_length=25,
     )
-    dobject = models.ForeignKey(Record, on_delete=models.PROTECT)
-
+    dobject = models.ForeignKey('Record',
+        related_name='relation_direct_object',
+        on_delete=models.PROTECT,
+    )
+    properties = pg.JSONField()
 
 
 class Record(Base, Sourced):
+    class Meta:
+        ordering = [
+            '-created_on',
+        ]
 
-    # schema = 'http://schema.org/Thing'
+    schema = 'http://schema.org/Thing'
 
-    # basic
-    name = models.CharField(
-        max_length=250,
+    CATEGORIES = {
+        'event': [
+            'course',
+            'exhibition',
+            'performance',
+            'reception',
+            'residency',
+            'workshop',
+        ],
+        'work': [
+            'article',
+            'book',
+            'installation',
+            'photograph',
+            'sculpture',
+            'visual artwork',
+            'website',
+            'vocabulary',
+            'license',
+        ],
+        'person': [
+            'artist',
+            'writer',
+            'architect',
+            'filmmaker',
+            'curator',
+            'gallerist',
+            'professor',
+        ],
+        'organization': [
+            'archive',
+            'association',
+            'company',
+            'consortium',
+            'foundation',
+            'library',
+            'museum',
+            'school',
+        ],
+        'page': [
+            'article',
+            'review',
+        ],
+        'collection': [
+            'tour'
+        ],
+    }
+
+    LABELS = (
+        (('event'), ('event')),
+        (('work'), ('work')),
+        (('person'), ('person')),
+        (('organization'), ('organization')),
+        (('page'), ('page')),
+        (('collection'), ('collection')),
     )
+
+    related = models.ManyToManyField('self',
+        through='Relation',
+        through_fields=('subject', 'dobject'),
+        symmetrical=False,
+        blank=True
+    )
+
+    # classification
     slug = models.SlugField(
-        unique=True
+        unique=True,
     )
-    description = models.ForeignKey('Snippet')
-    same_as = models.URLField(
+    body = models.TextField(
         blank=True,
     )
-    start = models.DateTimeField()
-    end = models.DateTimeField(
+    url = models.URLField(
         blank=True,
     )
-
-    # properties
     # categories = models.ManyToManyField('self',
     #     through='Relation', through_fields=('subject', 'dobject'),
     # )
-    labels = models.ManyToManyField('Label')
+    label = models.CharField(
+        max_length=25,
+        choices=LABELS,
+    )
     tags = models.ManyToManyField('Tag',
         blank=True,
     )
     images = models.ManyToManyField('Image',
         blank=True,
     )
+    license = models.ForeignKey('Resource',
+        related_name='records_licensed',
+    )
+
+    # common
+    lifespan = pg.DateRangeField()
+    duration = pg.DateTimeRangeField()
 
     # meta
-    license = models.ForeignKey('self',
-        related_name='licensed_records'
-    )
-    status = models.NullBooleanField(
+    version = models.CharField(
+        max_length=250,
         blank=True,
     )
-    featured = models.BooleanField(
+    is_active = models.NullBooleanField(
+        blank=True,
+        default=None,
+    )
+    is_featured = models.BooleanField(
         default=False,
     )
-    approved = models.BooleanField(
+    is_approved = models.BooleanField(
         default=False,
     )
-    web_public = models.BooleanField(
+    is_web_public = models.BooleanField(
         default=True,
     )
 
@@ -370,30 +405,40 @@ class Record(Base, Sourced):
     )
 
     # event fields
-    organizers = models.ManyToManyField('self',
-        through='Relation', through_fields=('subject', 'dobject'),
-        blank=True
+    is_group_friendly = models.NullBooleanField(
+        blank=True,
     )
-    curators = models.ManyToManyField('self',
-        through='Relation', through_fields=('subject', 'dobject'),
-        blank=True
+
+    # work fields
+    published_on = models.DateField(
+        blank=True,
     )
-    producers = models.ManyToManyField('self',
-        through='Relation', through_fields=('subject', 'dobject'),
-        blank=True
+
+    # person fields
+    GENDERS = (
+        ('m', 'male'),
+        ('f', 'female'),
+        ('x', 'x'),
     )
-    exhibitors = models.ManyToManyField('self',
-        through='Relation', through_fields=('subject', 'dobject'),
-        blank=True
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDERS,
+        blank=True,
     )
-    contributors = models.ManyToManyField('self',
-        through='Relation', through_fields=('subject', 'dobject'),
-        blank=True
+
+    # organization fields
+    is_nonprofit = models.NullBooleanField(
+        blank=True,
+        default=None,
+    )
+    by_appointment_only = models.NullBooleanField(
+        blank=True,
+        default=None,
     )
 
     # methods
     def __str__(self):
-        return '{}'.format(self.slug)
+        return '{}'.format(self.name)
 
     def name():
         pass
@@ -414,6 +459,9 @@ class Record(Base, Sourced):
         pass
 
     def distance():
+        pass
+
+    def duration():
         pass
 
 
